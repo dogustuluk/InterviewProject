1dd21637317328f6e43e8d3528edeed57e67eab3
dogustuluk <dogus.tuluk@gmail.com>
2022-08-19 22:31:31 +0300

    IResult and ResultStatus created

---
+++ MegBilisim.Shared/Utilities/Results/Abstract/IResult.cs
@@ -0,0 +1,16 @@
+﻿using MegBilisim.Shared.Utilities.Results.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegBilisim.Shared.Utilities.Results.Abstract
+{
+    public interface IResult
+    {
+        public ResultStatus ResultStatus { get;}
+        public string Message { get;}
+        public Exception Exception { get;}
+    }
+}
