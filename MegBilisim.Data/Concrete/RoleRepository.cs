9f3a1fef3c713abd8b9601331a36bdc4ce296ed8
dogustuluk <dogus.tuluk@gmail.com>
2022-08-19 15:47:18 +0300

    data -> Concrete classes are created

---
+++ MegBilisim.Data/Concrete/RoleRepository.cs
@@ -0,0 +1,15 @@
+﻿using MegBilisim.Data.Abstract;
+using MegBilisim.Entities.Concrete;
+using MegBilisim.Shared.Data.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegBilisim.Data.Concrete
+{
+    public class RoleRepository : EfEntityRepository<Role>, IRoleRepository
+    {
+    }
+}
---
+++ MegBilisim.Data/Concrete/WaterRepository.cs
@@ -0,0 +1,20 @@
+﻿using MegBilisim.Data.Abstract;
+using MegBilisim.Entities.Concrete;
+using MegBilisim.Shared.Data.Concrete.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegBilisim.Data.Concrete
+{
+    public class WaterRepository : EfEntityRepository<Water>, IWaterRepository
+    {
+        public WaterRepository(DbContext context) : base(context)
+        {
+            //Ek Özellikler
+        }
+    }
+}
