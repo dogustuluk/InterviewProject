fba72c0b10f0ab83b0a4a8b017f975766c7e4af2
dogustuluk <dogus.tuluk@gmail.com>
2022-08-19 17:52:29 +0300

    SubscribersMap

---
+++ MegBilisim.Data/Concrete/EntityFramework/Mappings/SubscriberMap.cs
@@ -0,0 +1,25 @@
+﻿using MegBilisim.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegBilisim.Data.Concrete.EntityFramework.Mappings
+{
+    public class SubscriberMap : IEntityTypeConfiguration<Subscribe>
+    {
+        public void Configure(EntityTypeBuilder<Subscribe> builder)
+        {
+            builder.HasKey(s => s.Id);
+            builder.Property(s => s.Id).ValueGeneratedOnAdd();
+            builder.HasOne<Company>(s => s.Company).WithMany(c => c.Subscriptions).HasForeignKey(s => s.CompanyId);
+            builder.HasOne<Dues>(s => s.Dues).WithMany(d => d.Subscribes).HasForeignKey(s => s.DuesId);
+            builder.HasOne<Electric>(s => s.Electric).WithMany(e => e.Subscribes).HasForeignKey(s => s.ElectricId);
+            builder.HasOne<Water>(s => s.Water).WithMany(w => w.Subscribes).HasForeignKey(s => s.WaterId);
+            builder.ToTable("Subscribers");
+        }
+    }
+}
