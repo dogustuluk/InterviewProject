d58e97540913045295e4d7ed70f292f5aabb0137
dogustuluk <dogus.tuluk@gmail.com>
2022-08-19 13:40:45 +0300

    EntityInvoiceBase,EntityUserBase,IEntity created

---
+++ MegBilisim.Shared/Entities/Abstract/EntityInvoiceBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegBilisim.Shared.Entities.Abstract
+{
+    public class EntityInvoiceBase
+    {
+        public virtual int Id { get; set; }
+        public virtual string SubscriberNo { get; set; }
+        public virtual bool CounterStatus { get; set; } = true;
+        public virtual DateTime PaymentDate { get; set; }
+        public virtual decimal PaymentTotal { get; set; }
+        public virtual DateTime CreatedDate { get; set; } = DateTime.Now;
+        public virtual DateTime ModifiedDate { get; set; } = DateTime.Now;
+        public virtual string CreatedByName { get; set; } = "Admin";
+        public virtual string ModifiedByName { get; set; } = "Admin";
+        public virtual bool IsDeleted { get; set; } = false;
+        public virtual string Note { get; set; }
+
+    }
+}
